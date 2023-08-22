1d4829392791c7fd91ca9de9b92698b1dbd64568
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2023-08-22 15:56:52 +0300

    devam

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
-﻿
+﻿string deneme = "kadirin deneme stringi";
+var istrue = deneme.Contains("k");
+if (istrue == true)
+{
+    Console.WriteLine("calisti");
+}
+
+
+int[] yenidizi = {1,2,3,563,2};
+Console.WriteLine(yenidizi[1]) ;
+
+string[] yazimdizisi = {"kadir", "ali", "veli"};
+Array.Sort(yazimdizisi);
+Console.WriteLine(yazimdizisi.Length);
+
+Console.WriteLine(yazimdizisi[0]);
+
+int[] soruDizisi = {1,2,3,423,43,5};
+
+foreach ( var item in soruDizisi)
+{
+Console.WriteLine(item);
+}
+
 namespace deneme
 {
 
@@ -12,13 +35,18 @@ namespace deneme
             ogr1.adSoyad = " hatay";
             ogr1.sube = "2a";
 
-            Console.WriteLine($"{ogr1.ogrenciNo}numaralı ogrenci");
+            Console.WriteLine(ogr1.PrintInformation());
         }
     }
     class Ogrenci
     {
-        public string ogrenciNo { get; set; }
+        public string ogrenciNo { get; set; }// nesneye özel değil doğrudan sınıf üzerinden çağırılan bir metod yazmak için static kullanılır.
         public string adSoyad { get; set; }
         public string sube { get; set; }
+
+        //methods
+        public string PrintInformation(){
+            return $"{this.ogrenciNo}numaralı ogrenci" ;  // this: o an erişilen nesne
+        }
     }
 }
