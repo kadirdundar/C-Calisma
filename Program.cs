e69c41a971b80bc7706ccacbaa7de9c93ed1c5a8
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2023-08-22 15:55:47 +0300

    genel isleyis

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace deneme
+{
+
+    class Program
+    {
+        static void Main(string[] args){
+
+            Ogrenci ogr1 = new Ogrenci();
+
+            ogr1.ogrenciNo = "123";
+            ogr1.adSoyad = " hatay";
+            ogr1.sube = "2a";
+
+            Console.WriteLine($"{ogr1.ogrenciNo}numaralı ogrenci");
+        }
+    }
+    class Ogrenci
+    {
+        public string ogrenciNo { get; set; }
+        public string adSoyad { get; set; }
+        public string sube { get; set; }
+    }
+}
